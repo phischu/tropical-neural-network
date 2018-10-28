bc6712e4cbacb7e4aa00c8ba779605072cbdcd9f
Philipp Schuster <philipp.schuster@uni-tuebingen.de>
2018-10-28 17:49:53 +0100

    Render output of net.

--- src/Main.hs
+++ src/Main.hs
@@ -8,9 +8,9 @@ import Test.QuickCheck.Gen (
   Gen(MkGen))
 
 import Diagrams.Prelude (
-  Diagram, circle, position, rect,
-  atop,
-  fillColor, lineColor, orange, blue, white, transparent,
+  Diagram, circle, position, rect, rasterDia,
+  atop, scaleUToX,
+  fillColor, lineColor, orange, blue, white, transparent, opaque, blend,
   dims)
 import Diagrams.Backend.Rasterific (
   renderRasterific, Rasterific)
@@ -19,6 +19,9 @@ import Linear (
   V2(V2))
 import Linear.Affine (
   Point(P))
+import Linear.Matrix (
+  M22, identity, (!*))
+
 import Control.Monad (
   replicateM)
 import Data.Function (
@@ -64,19 +67,54 @@ renderTrainingSamples :: [TrainingSample] -> Diagram Rasterific
 renderTrainingSamples trainingSamples = position (do
   TrainingSample x y <- trainingSamples
   let yColor = case y of
-        False -> orange
-        True -> blue
+        False -> blue
+        True -> orange
   let sampleDiagram = circle 0.02 & fillColor yColor
   return (P x, sampleDiagram))
+
+
+
+data Net a = Net (M22 a)
+
+runNet :: (Fractional a) => Net a -> V2 a -> a
+runNet (Net m1) x = sum (m1 !* x)
+
+
+exampleNet :: (Num a) => Net a
+exampleNet = Net identity
+
+
+renderNet :: Net Double -> Diagram Rasterific
+renderNet net = let
+
+  resolution = 600
+
+  outputColor r =
+    opaque (blend (logistic r) orange blue)
+
+  logistic x = recip (1 + exp (negate x))
+
+  pointRaster i j = let
+    x1 = 2 * (fromIntegral i / fromIntegral resolution) - 1
+    x2 = negate (2 * (fromIntegral j / fromIntegral resolution) - 1)
+    in V2 x1 x2
+
+  pointColor i j =
+    outputColor (runNet net (pointRaster i j))
+
+  in rasterDia pointColor resolution resolution & scaleUToX 2
 
 
 main :: IO ()
 main = do
   let whiteBackground =
         rect 2.1 2.1 & fillColor white & lineColor transparent
-  let samplesDiagram =
-        renderTrainingSamples trainingData `atop` whiteBackground
-  let sizeSpec = dims (V2 600 600)
-  renderRasterific "training_samples.png" sizeSpec samplesDiagram
+  let outputDiagram =
+        renderTrainingSamples trainingData `atop`
+        renderNet exampleNet `atop`
+        whiteBackground
+  let sizeSpec =
+        dims (V2 600 600)
+  renderRasterific "training_samples.png" sizeSpec outputDiagram
 
 
